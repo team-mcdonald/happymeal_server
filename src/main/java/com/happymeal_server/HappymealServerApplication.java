040beb36f4d0856e5b2736f924637a55be804fc1
whitebear05 <woojs0505@naver.com>
2023-01-14 18:23:40 +0900

    Initial commit

---
+++ src/main/java/com/happymeal_server/HappymealServerApplication.java
@@ -0,0 +1,13 @@
+package com.happymeal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappymealServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappymealServerApplication.class, args);
+    }
+
+}
